bbfc3ff5ddb0f7f345d913c8c8036aace0687eef
stormbels <lizarjevskaya@gmail.com>
2026-06-26 13:25:12 +0300

    Complete day00 exercises 07-09

---
+++ day00/src/ex09/day00_ex09.sql
@@ -0,0 +1,6 @@
+SELECT
+    (SELECT name FROM person WHERE id = person_visits.person_id) AS person_name,
+    (SELECT name FROM pizzeria WHERE id = person_visits.pizzeria_id) AS pizzeria_name
+FROM person_visits
+WHERE visit_date BETWEEN '2022-01-07' AND '2022-01-09'
+ORDER BY person_name ASC, pizzeria_name DESC;
